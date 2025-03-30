bf49424656a3453fa49e004643233f86a2c33bb5
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-30 22:58:07 +0200

    Add project files.

---
+++ Jolly Pirate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jolly_Pirate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
