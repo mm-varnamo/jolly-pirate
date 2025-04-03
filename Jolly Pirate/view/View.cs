9db7f5715451d7dabfb738e7d698806850bd98fd
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-03 22:08:50 +0200

    feat: add RenderMainMenu method for displaying main menu

--- Jolly Pirate/view/View.cs
+++ Jolly Pirate/view/View.cs
@@ -23,7 +23,7 @@ namespace Jolly_Pirate.view
             None
         }
 
-        public void renderLogo()
+        public void RenderLogo()
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
@@ -33,6 +33,26 @@ namespace Jolly_Pirate.view
             Console.WriteLine("X                      X");
             Console.WriteLine("X======================X");
             Console.ResetColor();
+        }
+
+        public void RenderMainMenu()
+        {
+            RenderLogo();
+            Console.WriteLine("1. Register a new memeber");
+            Console.WriteLine("2. Edit a member");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("3. Delete a member");
+            Console.ResetColor();
+            Console.WriteLine("4. Register a boat to a member");
+            Console.WriteLine("5. Edit a members boat");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("6. Delete a members boat");
+            Console.ResetColor();
+            Console.WriteLine("7. View a simple list of members");
+            Console.WriteLine("8. View a detailed list of members");
+            Console.WriteLine("9. View a specific members information");
+            Console.WriteLine("0. Quit the program");
+
         }
     }
 }
