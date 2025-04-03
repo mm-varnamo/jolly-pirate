7fe2b6f50b642ffee60a76ace7432a4a89c03068
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-03 22:02:29 +0200

    feat: add ActionTaken enum for user commands

---
+++ Jolly Pirate/view/View.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jolly_Pirate.view
+{
+    public class View
+    {
+        public enum ActionTaken
+        {
+            RegisterMember,
+            EditMember,
+            DeleteMember,
+            RegisterBoat,
+            EditBoat,
+            DeleteBoat,
+            ViewSimpleList,
+            ViewDetailedList,
+            ViewSpecificMemeber,
+            Quit,
+            None
+        }
+    }
+}
