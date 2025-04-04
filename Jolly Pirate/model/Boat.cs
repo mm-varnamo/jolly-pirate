46ad60268b1205bcad5cf5e11efcb5746014ab00
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-04 20:55:46 +0200

    feat: add Boat class with mutable properties and validation

---
+++ Jolly Pirate/model/Boat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jolly_Pirate.model
+{
+    public enum BoatType
+    {
+        Canoe,
+        BattleShip,
+        Yacht,
+        SubMarine
+    }
+    public class Boat
+    {
+        private int _length;
+        private BoatType _type;
+        public Boat(int length, BoatType type)
+        {
+            Length = length;
+            Type = type;
+        }
+
+        public int Length
+        {
+            get => _length;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Boat length must be greater than zero.");
+                }
+                else
+                {
+                    _length = value;
+                }
+
+            }
+        }
+
+        public BoatType Type
+        {
+
+            get => _type;
+            set
+            {
+                if (!Enum.IsDefined(typeof(BoatType), value))
+                {
+                    throw new ArgumentOutOfRangeException("Invalid boat type provided.");
+                }
+                else
+                {
+                    _type = value;
+                }
+            }
+        }
+    }
+}
