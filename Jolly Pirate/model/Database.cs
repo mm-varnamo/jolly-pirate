459d5a51d6806156a5d612a8187b911d16fb2326
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-07 16:34:31 +0200

    feat: implement members list save/load with JSON serialization

---
+++ Jolly Pirate/model/Database.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Jolly_Pirate.model
+{
+    public class Database
+    {
+        private string _fileName = "MembersRegistry.json";
+
+        public void SaveMembersRegistryToDB(IEnumerable<Member> members)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(members, options);
+            File.WriteAllText(_fileName, jsonString);
+        }
+
+        public IEnumerable<Member> LoadMembersRegistryFromDB()
+        {
+            if (!File.Exists(_fileName)) return new List<Member>();
+
+            try
+            {
+                string jsonString = File.ReadAllText(_fileName);
+                return JsonSerializer.Deserialize<List<Member>>(jsonString) ?? new List<Member>();
+            }
+            catch
+            {
+                return new List<Member>();
+            }
+        }
+    }
+}
