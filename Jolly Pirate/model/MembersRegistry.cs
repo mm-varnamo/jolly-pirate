f3013720e4dbaa2e0c84236f143df683e06e4256
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-08 20:33:08 +0200

    feat: add MembersRegistry class for managing member list

---
+++ Jolly Pirate/model/MembersRegistry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jolly_Pirate.model
+{
+    public class MembersRegistry
+    {
+        private List<Member> _members;
+        public MembersRegistry(Database database) {
+            _members = database.LoadMembersRegistryFromDB();
+        }
+        public void AddMember(Member member)
+        {
+            _members.Add(member);
+        }
+        public void DeleteMember(Guid ID)
+        {
+            int deletedAmount = _members.RemoveAll(m => m.UniqueID == ID);
+
+            if (deletedAmount == 0) {
+                throw new ArgumentOutOfRangeException("A member with that id does not exist!");
+            }
+        }
+        public Member GetMemberByID(Guid ID)
+        {
+
+            var member = _members.Find(m => m.UniqueID == ID);
+
+            if (member == null)
+            {
+                throw new ArgumentOutOfRangeException("A member with that id does not exist!");
+            }
+
+            return member;
+        }
+        public IEnumerable<Member> GetMembersList()
+        {
+            return _members;
+        }
+    }
+}
