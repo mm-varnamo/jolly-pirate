0b968cb73896aa57d5b687e2e51a39d9993573ca
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-04-13 19:56:36 +0200

    feat(program): implement app Run code

--- Jolly Pirate/Program.cs
+++ Jolly Pirate/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Jolly_Pirate
+﻿using Jolly_Pirate.controller;
+using Jolly_Pirate.model;
+using Jolly_Pirate.view;
+
+namespace Jolly_Pirate
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Database db = new Database();
+            MembersRegistry m = new MembersRegistry(db);
+            View v = new View();
+            Controller c = new Controller(db);
+
+            try
+            {
+                c.Run(v, m);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unfortunately something unexpected happened!");
+                Console.WriteLine("Please restart the application!");
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            Console.ReadKey();
         }
     }
 }
